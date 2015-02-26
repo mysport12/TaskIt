733bd50bf7d5c49228c408ec984e1264b7f8399b
Craig Martin <cmart1220@gmail.com>
2015-02-25 23:03:42 -0500

    Initial Commit

---
+++ TaskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt
+//
+//  Created by Craig Martin on 2/25/15.
+//  Copyright (c) 2015 MadKitty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
